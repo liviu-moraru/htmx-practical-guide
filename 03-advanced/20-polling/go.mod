fbcd995f1931fd3c6f77b1e9ddf745ef3534b92e
liviu-moraru <liviu.moraru@gmail.com>
2024-08-05 08:09:53 +0300

    section 3. 20-polling

---
+++ 03-advanced/20-polling/go.mod
@@ -0,0 +1,3 @@
+module htmx.polling
+
+go 1.22.3
---
+++ 03-advanced/20-polling/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("POST /places", app.places)
+	mux.HandleFunc("DELETE /places/{id}", app.delete)
+	mux.HandleFunc("GET /suggested-locations", app.suggestedLocations)
+	return mux
+}
