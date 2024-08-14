707fc1f3f0f5a72333ead2cab16fb917de11b1c1
liviu-moraru <liviu.moraru@gmail.com>
2024-08-14 18:27:23 +0300

    section 4. 06-retarget-reswap

---
+++ 04-requests-responses/06-retarget-reswap/go.mod
@@ -0,0 +1,5 @@
+module htmx.retarget.reswap
+
+go 1.22.3
+
+require github.com/justinas/nosurf v1.1.1 // indirect
---
+++ 04-requests-responses/06-retarget-reswap/handlers.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"github.com/justinas/nosurf"
+	"html/template"
+	"math/rand/v2"
+	"net/http"
+	"strings"
+)
+
+const (
+	homePageTemplate      = "./ui/html/pages/home.tmpl"
+	loginFragmentTemplate = "./ui/html/fragments/login.tmpl"
+	authenticatedTemplate = "./ui/html/pages/authenticated.tmpl"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := getTemplate(homePageTemplate)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	token := nosurf.Token(r)
+
+	err = tmpl.Execute(w, struct{ Token string }{token})
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func (app *application) validate(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		app.clientError(w, r, http.StatusBadRequest, err)
+		return
+	}
+
+	t := "{{.Error}}"
+	tmpl, err := template.New("validate").Parse(t)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	var text string
+
+	email := r.PostForm.Get("email")
+
+	if email != "" {
+		if !strings.Contains(email, "@") {
+			text = "E-Mail address is invalid."
+		}
+		err := tmpl.Execute(w, struct{ Error string }{Error: text})
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+		return
+	}
+
+	password := r.PostForm.Get("password")
+	if password != "" {
+		if len(strings.TrimSpace(password)) < 8 {
+			text = "Password must be at least 8 characters long."
+		}
+		err := tmpl.Execute(w, struct{ Error string }{Error: text})
+		if err != nil {
+			app.serverError(w, r, err)
+		}
+	}
+}
+
+func (app *application) login(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		app.clientError(w, r, http.StatusBadRequest, err)
+		return
+	}
+	email := r.PostForm.Get("email")
+	password := r.PostForm.Get("password")
+	var errors []string
+	if email == "" || !strings.Contains(email, "@") {
+		errors = append(errors, "Please enter a valid email address.")
+	}
+	if password == "" || len(strings.TrimSpace(password)) < 8 {
+		errors = append(errors, "Password must be at least 8 characters long.")
+	}
+
+	if len(errors) == 0 {
+		if rand.Float64() > 0 {
+			w.Header().Set("HX-Retarget", ".control")
+			w.Header().Set("HX-Reswap", "beforebegin")
+			t := template.Must(template.New("serverError").Parse(`
+		<p class="error">A server-side error occurred. Please try again.</p>
+`))
+			err := t.Execute(w, nil)
+			if err != nil {
+				app.serverError(w, r, err)
+			}
+			return
+		}
+		w.Header().Set("HX-Redirect", "/authenticated")
+		return
+	}
+
+	tmpl, err := getTemplate(loginFragmentTemplate)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	err = tmpl.Execute(w, struct{ Errors []string }{Errors: errors})
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func (app *application) authenticated(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := getTemplate(authenticatedTemplate)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
+
+func getTemplate(templateFiles ...string) (*template.Template, error) {
+	return template.ParseFiles(templateFiles...)
+}
